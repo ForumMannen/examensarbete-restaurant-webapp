52ce00e4f0612c6b187a3ab5c50753e6c33c9c0e
Linus Lundell <lundell.linus@gmail.com>
2024-01-17 10:09:06 +0100

    rendered the products and sorted them by categories

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,28 +1,44 @@
 import { useMenuData } from "../hooks/fetchMenuList";
 import './Home.css';
+import { IRecipesData } from "../hooks/fetchMenuList";
 
 const Home: React.FC = () => {
   const { menuData } = useMenuData();
   const { recipes } = menuData;
   console.log(recipes);
+
+  const sortRecipesByCategory: Record<string, IRecipesData[]> = {};
+
+  recipes.forEach(recipe => {
+    if (!sortRecipesByCategory[recipe.category]) {
+      sortRecipesByCategory[recipe.category] = [];
+    }
+    sortRecipesByCategory[recipe.category].push(recipe);
+  });
   
-  
-  return (
-    <>
+  // Iterate through categories and create tables
+  const categoryTables = Object.keys(sortRecipesByCategory).map((category: string) => (
+    <div key={category}>
+      <h2>{category}</h2>
       <div className="table">
-        {recipes.map((recipe) => (
-          <article className="product-card" key={recipe._id}>
-            <div className="product-info">
+        {sortRecipesByCategory[category].map((recipe, index) => (
+          <article className="product-card" key={index}>
+            <a href="/product-page" className="product-info">
               <p className="product-title">{recipe.name}</p>
-              <p className="product-ingredients">{recipe.category}</p>
+              <div className="product-modifiers">
+                {recipe.modifiers.map((modifier, modifierIndex) => (
+                  <p key={modifierIndex}>{modifier.name}</p>
+                ))}
+              </div>
               <p className="product-price">{recipe.price} kr</p>
-              <button className="add-to-cart">Lägg i varukorg</button>
-            </div>
+            </a>
           </article>
         ))}
       </div>
-    </>
-  )
+    </div>
+  ));
+
+  return <>{categoryTables}</>;
 }
 
 export default Home
